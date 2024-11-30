e4c8b3a2bc253e426a6260a56447dbeac93b63e8
Binh707 <hoanhbinhqt@gmail.com>
2024-11-30 21:24:51 +0700

    Update criterion.py

--- embryo_former/criterion.py
+++ embryo_former/criterion.py
@@ -138,39 +138,39 @@ class SetCriterion(nn.Module):
 
 
 
-    # def loss_masks(self, masks_queries_logits, targets, indices, num_boxes):
-    #     """Compute the losses related to the masks using sigmoid_cross_entropy_loss and dice loss."""
+    def loss_masks(self, masks_queries_logits, targets, indices, num_boxes):
+        """Compute the losses related to the masks using sigmoid_cross_entropy_loss and dice loss."""
 
-    #     B, L, Qn = masks_queries_logits.shape
+        B, L, Qn = masks_queries_logits.shape
 
-    #     indices, many2one_indices = indices
-    #     assert 'pred_logits' in outputs
-    #     idx = self._get_src_permutation_idx(indices)
-    #     pred_masks = masks_queries_logits[idx]
+        indices, many2one_indices = indices
+        assert 'pred_logits' in outputs
+        idx = self._get_src_permutation_idx(indices)
+        pred_masks = masks_queries_logits[idx]
 
-    #     stage_classes = torch.cat([t["labels"][J] for t, (_, J) in zip(targets, indices)]) #[bs, ]
+        stage_classes = torch.cat([t["labels"][J] for t, (_, J) in zip(targets, indices)]) #[bs, ]
 
-    #     frame_gts = []
-    #     for t in targets:
-    #         t_gts = t['frame_labels']
-    #         t_len = t['frame_labels'].shape[0]
-    #         pad_labels = torch.tensor([self.num_classes]*(L - t_len)).to(t['frame_labels'].device)
-    #         t_gts = torch.cat([t['frame_labels'], pad_labels])
-    #         frame_gts.append(t_gts)
-    #     frame_gts = torch.cat(frame_gts) # [bs, seq len]
+        frame_gts = []
+        for t in targets:
+            t_gts = t['frame_labels']
+            t_len = t['frame_labels'].shape[0]
+            pad_labels = torch.tensor([self.num_classes]*(L - t_len)).to(t['frame_labels'].device)
+            t_gts = torch.cat([t['frame_labels'], pad_labels])
+            frame_gts.append(t_gts)
+        frame_gts = torch.cat(frame_gts) # [bs, seq len]
 
-    #     # target_masks = torch.cat[frame_gts[i] == stage_classes[i] for ]
+        # target_masks = torch.cat[frame_gts[i] == stage_classes[i] for ]
 
-    #     # No need to upsample predictions as we are using normalized coordinates
-    #     pred_masks = pred_masks[:, None]
-    #     target_masks = target_masks[:, None]
+        # No need to upsample predictions as we are using normalized coordinates
+        pred_masks = pred_masks[:, None]
+        target_masks = target_masks[:, None]
 
-    #     losses = {
-    #         "loss_mask": sigmoid_cross_entropy_loss(point_logits, point_labels, num_boxes),
-    #         "loss_dice": dice_loss(point_logits, point_labels, num_boxes),
-    #     }
+        losses = {
+            "loss_mask": sigmoid_cross_entropy_loss(point_logits, point_labels, num_boxes),
+            "loss_dice": dice_loss(point_logits, point_labels, num_boxes),
+        }
 
-    #     return losses
+        return losses
 
 
 
