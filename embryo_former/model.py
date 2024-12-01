9868b8e4957b46c61342cae00e0362eeb3a13b87
Binh707 <hoanhbinhqt@gmail.com>
2024-12-01 22:04:22 +0700

    Update model.py

--- embryo_former/model.py
+++ embryo_former/model.py
@@ -227,10 +227,9 @@ class EmbryoFormer(nn.Module):
         if self.aux_loss:
             ks, vs = list(zip(*(all_out.items())))
             out['aux_outputs'] = [{ks[i]: vs[i][j] for i in range(len(ks))} for j in range(num_pred - 1)]
+            loss, last_indices, aux_indices = criterion(out, dt['video_target'])
         else:
             loss, last_indices = criterion(out, dt['video_target'])
-
-        loss, last_indices, aux_indices = criterion(out, dt['video_target'])
         return out, loss
 
 
